b27f761d38d36a9659df1f9da5f292d508e0d285
ajaysehwal <ajaysehwal000@gmail.com>
2024-09-07 10:42:20 +0530

    initial commit

---
+++ contracts/target/debug/deps/contracts-19821f3bda49e04c.d
@@ -0,0 +1,5 @@
+C:\Users\ajays\code\examchain\contracts\target\debug\deps\libcontracts-19821f3bda49e04c.rmeta: src/main.rs
+
+C:\Users\ajays\code\examchain\contracts\target\debug\deps\contracts-19821f3bda49e04c.d: src/main.rs
+
+src/main.rs:
